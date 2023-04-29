ffe828759ff30c4636b92df0dbda2f464e6cae13
Anoop Sharma <anoopsharma527@gmail.com>
2023-04-30 00:47:12 +0530

    Added ChatGPT support for nlu_fallback

--- actions/actions.py
+++ actions/actions.py
@@ -4,24 +4,55 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
+from typing import Any, Text, Dict, List
 
-# This is a simple example for a custom action which utters "Hello World!"
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import UserUtteranceReverted
+import requests
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+
+class ActionDefaultFallback(Action):
+    def name(self) -> Text:
+        return "action_default_fallback"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+    
+    # Get user message from Rasa tracker
+        user_message = tracker.latest_message.get('text')
+        print(user_message)
+
+    # def get_chatgpt_response(self, message):
+        url = 'https://api.openai.com/v1/chat/completions'
+        headers = {
+            'Authorization': 'Bearer ADD_YOUR_OPENAI_API_KEY_HERE',
+            'Content-Type': 'application/json'
+        }
+        data = {
+            'model': "gpt-3.5-turbo",
+            'messages': [   {'role': 'system', 'content': 'You are an AI assistant for the user. You help to solve user query'}
+                            {'role': 'user', 'content': 'You: ' + user_message}
+                            ],
+            'max_tokens': 100
+        }
+        response = requests.post(url, headers=headers, json=data)
+                # response = requests.post(api_url, headers=headers, json=data)
+
+        if response.status_code == 200:
+            chatgpt_response = response.json()
+            message = chatgpt_response['choices'][0]['message']['content']
+            dispatcher.utter_message(message)
+        else:
+            # Handle error
+            return "Sorry, I couldn't generate a response at the moment. Please try again later."
+        
+                # Revert user message which led to fallback.
+        return [UserUtteranceReverted()]
+      
+      
+      
